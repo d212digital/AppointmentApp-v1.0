c8bf663bcf7dc1582abd50d46b1847030d6ba51b
David Adamson <david@212digital.co.uk>
2021-03-31 11:19:47 +0100

    Add project files.

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,26 @@
+﻿using AppointmentApp.Data;
+using AppointmentApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentApp.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public ItemController(ApplicationDBContext db)
+        {
+            _db = db;   
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Item> objList = _db.Items;
+            return View(objList);
+        }
+    }
+}
---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using AppointmentApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentApp.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) 
+        { 
+            
+        }
+        public DbSet<Item> Items { get; set; }
+    }
+
+}
